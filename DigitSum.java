a89ef56c277efd03c4ea32894f334c3cd1a914e5
bharat <47022219+Hiremath19@users.noreply.github.com>
2019-03-06 16:45:21 +0530

    Add files via upload

---
+++ DigitSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DigitSum
+{
+    public static void main(String args[])
+    {
+        System.out.println("enter the number");
+        Scanner sc=new Scanner(System.in);
+        String num=sc.nextLine();
+       int digitSum=0;
+       for(int i=0;i<num.length();i++)
+       {
+
+       }
+
+
+    }
+}
